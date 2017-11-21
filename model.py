56a33421e9e31097b304d990bf6e9483b338ab77
Kathrine94 <dhn@mail.ustc.edu.cn>
2017-11-21 17:35:05 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -4,17 +4,6 @@ import torch.utils.model_zoo as model_zoo
 import torch
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152']
-
-
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -185,6 +174,7 @@ def mean_image_subtraction(images, means=[123.68, 116.78, 103.94]):
       raise ValueError('len(means) must match the number of channels')
     for i in range(num_channels):
         images.data[:,i,:,:] -= means[i]
+
     return images
 
 class East(nn.Module):
@@ -192,43 +182,81 @@ class East(nn.Module):
         super(East, self).__init__()
         self.resnet = resnet50(True)
         self.conv1 = nn.Conv2d(3072, 128, 1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu1 = nn.ReLU()
+
         self.conv2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+
         self.conv3 = nn.Conv2d(640, 64, 1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
+
         self.conv4 = nn.Conv2d(64, 64, 3 ,padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU()
+
         self.conv5 = nn.Conv2d(320, 64, 1)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.relu5 = nn.ReLU()
+
         self.conv6 = nn.Conv2d(64, 32, 3, padding=1)
+        self.bn6 = nn.BatchNorm2d(32)
+        self.relu6 = nn.ReLU()
+
         self.conv7 = nn.Conv2d(32, 32, 3, padding=1)
+        self.bn7 = nn.BatchNorm2d(32)
+        self.relu7 = nn.ReLU()
+
         self.conv8 = nn.Conv2d(32, 1, 1)
         self.sigmoid1 = nn.Sigmoid()
         self.conv9 = nn.Conv2d(32, 4, 1)
         self.sigmoid2 = nn.Sigmoid()
         self.conv10 = nn.Conv2d(32, 1, 1)
         self.sigmoid3 = nn.Sigmoid()
-        self.unpool = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.unpool1 = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.unpool2 = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.unpool3 = nn.Upsample(scale_factor=2, mode='bilinear')
     def forward(self,images):
         images = mean_image_subtraction(images)
         _, f = self.resnet(images)
         h = f[3]  # bs 2048 w/32 h/32
-        g = (self.unpool(h)) #bs 2048 w/16 h/16
+        g = (self.unpool1(h)) #bs 2048 w/16 h/16
         c = self.conv1(torch.cat((g, f[2]), 1))
+        c = self.bn1(c)
+        c = self.relu1(c)
         
         h = self.conv2(c)  # bs 128 w/16 h/16
-        g = self.unpool(h) # bs 128 w/8 h/8
+        h = self.bn2(h)
+        h = self.relu2(h)
+        g = self.unpool2(h) # bs 128 w/8 h/8
         c = self.conv3(torch.cat((g, f[1]), 1))
-        
+        c = self.bn3(c)
+        c = self.relu3(c)
+
         h = self.conv4(c)  # bs 64 w/8 h/8
-        g = self.unpool(h) # bs 64 w/4 h/4
+        h = self.bn4(h)
+        h = self.relu4(h)
+        g = self.unpool3(h) # bs 64 w/4 h/4
         c = self.conv5(torch.cat((g, f[0]), 1))
+        c = self.bn5(c)
+        c = self.relu5(c)
         
         h = self.conv6(c) # bs 32 w/4 h/4
+        h = self.bn6(h)
+        h = self.relu6(h)
         g = self.conv7(h) # bs 32 w/4 h/4
+        g = self.bn7(g)
+        g = self.relu7(g)
         
         F_score = self.conv8(g) #  bs 1 w/4 h/4
         F_score = self.sigmoid1(F_score)
         geo_map = self.conv9(g)
-        geo_map = self.sigmoid2(geo_map)
+        geo_map = self.sigmoid2(geo_map) * 512
         angle_map = self.conv10(g)
         angle_map = self.sigmoid3(angle_map)
-        
+        angle_map = (angle_map - 0.5) * math.pi / 2
+
         F_geometry = torch.cat((geo_map, angle_map), 1) # bs 5 w/4 w/4
         return F_score, F_geometry
